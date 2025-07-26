9598a587dfcbbf95ced7c08695f18dab7c59e4ac
pragyac09 <pragyac09@gmail.com>
2025-07-26 15:38:35 +0530

    Update AllTask.jsx

--- components/other/AllTask.jsx
+++ components/other/AllTask.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../../context/AuthProvider'; 
 
-
 const AllTask = () => {
-
+  
 const [userData, setUserData] = useContext(AuthContext)
+
   return (
     <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
         <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
@@ -15,7 +15,8 @@ const [userData, setUserData] = useContext(AuthContext)
             <h5 className='text-lg font-medium w-1/5 '>Failed</h5>
         </div>
         <div className=''>
-            {userData.employees.map(function(elem,idx){
+          
+          {userData.map(function(elem,idx){
             return <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2 className='text-lg font-medium w-1/5'>{elem.firstName}</h2>
             <h3 className='text-lg font-medium w-1/5 text-blue-400'>{elem.taskSummary.newTask}</h3>
@@ -24,10 +25,8 @@ const [userData, setUserData] = useContext(AuthContext)
             <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskSummary.failed}</h5>
         </div>
         })}
-       
         </div>
     </div>
   )
 }
-
 export default AllTask
