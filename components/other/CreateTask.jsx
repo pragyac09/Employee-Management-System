d364c5565d6fdf4eb0776379c971700b22e5faa8
pragyac09 <pragyac09@gmail.com>
2025-07-26 15:39:44 +0530

    Update CreateTask.jsx

--- components/other/CreateTask.jsx
+++ components/other/CreateTask.jsx
@@ -3,63 +3,54 @@ import NewTask from '../TaskList/NewTask'
 import { AuthContext } from '../../context/AuthProvider'
 import { useContext } from 'react'
 
-const CreateTask = (e) => {
-
+const CreateTask = () => {
 const [userData, setUserData] = useContext(AuthContext)
-
-    const [taskTitle, setTaskTitle] = useState('')
-    const [taskDescription, setTaskDescription] = useState('')
-    const [taskDate, setTaskDate] = useState('')
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [date, setDate] = useState('')
     const [assignTo, setAssignTo] = useState('')
     const [category, setCategory] = useState('')
 
-    const [task, setTask] = useState({})
-
-    const submitHandler=()=>{
+    const [newTask, setNewTask] = useState({})
+    const submitHandler = (e) => {
         e.preventDefault()
-
-        setTask({taskTitle, taskDescription, taskDate, category, active:false, NewTask:true, failed:true, completed:false})
-        
-        const data= JSON.parse(localStorage.getItem('employees'))
-
-        data.forEach(function(elem){
-            if(assignTo == elem.firstName){
-                elem.tasks.push(newTask)
-                elem.taskSummary.newTask= elem.taskSummary.newTask+1
-            }
-        })
-        setUserData(data)
-        
-
-        setTaskTitle('')
-        setCategory('')
-        setAssignTo('')
-        setTaskDate('')
-        setTaskDescription('')
-        
-    }
-
+        setNewTask({title,description,date,assignTo,category,active:false,completed:false,newTask:true,failed:false})
+    const data = userData
+    data.forEach(function(emp)  {
+    if (assignTo.toLowerCase() === emp.firstName.toLowerCase()) {
+        emp.tasks.push(newTask)
+        emp.taskSummary.newTask=emp.taskSummary.newTask+1
+    }})
+  setUserData(data)
+  localStorage.setItem('employees', JSON.stringify(data));
+  setTitle('')
+  setCategory('')
+  setAssignTo('')
+  setDate('')
+  setDescription('')
+  alert('Task successfully created!')
+}
   return (
     <div className='p-5 bg-[#1c1c1c] mt-7 rounded'>
-        <form onSubmit={(e)=>{
-            submitHandler(e)
-        }} className='flex flex-wrap w-full bg-transparent items-start justify-between'>
+        <form 
+        onSubmit={submitHandler}
+        className='flex flex-wrap w-full bg-transparent items-start justify-between'>
             <div className='w-1/2'>
                 <div>
                 <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
             <input 
-            value={taskTitle}
+            value={title}
             onChange={(e)=>{
-                setTaskTitle(e.target.value)
+                setTitle(e.target.value)
             }}
             className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Maje a deign' />
             </div>
                 <div>
                 <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
             <input
-            value={taskDate}
+            value={date}
             onChange={(e)=>{
-                setTaskDate(e.target.value)
+                setDate(e.target.value)
             }}
              className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type='date' />
             </div>
@@ -85,12 +76,17 @@ const [userData, setUserData] = useContext(AuthContext)
             
             <div className='w-1/2 flex flex-col items-start'> 
                 <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
-            <textarea className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-2'name="" id="" cols="30" rows="10"></textarea>
-            <button className='bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-5 w-full'>Create Task</button>
+            <textarea 
+                value={description}
+                onChange={(e)=>{
+                    setDescription(e.target.value)
+                }}
+            className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-2'name="" id="" cols="30" rows="10"></textarea>
+            <button 
+                type='submit'
+            className='bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-5 w-full'>Create Task</button>
              </div>
         </form>
     </div>
-  )
-}
-
+  )}
 export default CreateTask
