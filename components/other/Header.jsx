ea940f8fc2859cf9b9361b8f571f5723542c3a77
pragyac09 <pragyac09@gmail.com>
2025-07-26 15:41:58 +0530

    Update Header.jsx

--- components/other/Header.jsx
+++ components/other/Header.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { setLocalStorage } from '../../utils/localStorage'
 
 const Header = (props) => {
 
-    /*const [username, setusername] = useState('')
-    if(!data){
+    const [username, setusername] = useState('')
+    const data=props.data
+  
+    useEffect(()=>{
+        if(!data){
         setusername('Admin')
     }else{
         setusername(data.firstName)
-    }*/
+    }
+    },[data])
+    
 
-        const logOutUser =()=>{
+    const logOutUser =()=>{
                 localStorage.setItem('loggedInUser','')
                 props.changeUser('')
                 window.location.reload()
@@ -18,7 +23,7 @@ const Header = (props) => {
 
   return (
     <div className='flex items-end justify-between'>
-        <h1 className='text-2xl font-medium'>Hello <br /><span className='text-3xl font-semibold'>username👋</span></h1>
+        <h1 className='text-2xl font-medium'>Hello <br /><span className='text-3xl font-semibold'>{username}👋</span></h1>
         <button onClick={logOutUser} className='bg-red-500 text-lg font-medium px-5 text-white rounded-small'>Log Out</button>
     </div>
   )
