942a2e7fe4093a9d67db2e8433dfd81a6990c543
pragyac09 <pragyac09@gmail.com>
2025-07-26 15:42:59 +0530

    Update TaskListNumbers.jsx

--- components/other/TaskListNumbers.jsx
+++ components/other/TaskListNumbers.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-
 const TaskListNumbers = ({data}) => {
+    if (!data || !data.taskSummary) {
+    return (
+      <div className="text-white text-lg mt-10">Loading task summary...</div>
+    );}
   return (
     <div className='flex mt-10 justify-between gap-5 screen'>
         <div className='rounded-xl py-6 px-9 w[45%] p-10 bg-red-400'>
@@ -23,7 +26,5 @@ const TaskListNumbers = ({data}) => {
             <h3 className='text-xl font-medium'>Active Task</h3>
         </div>
     </div>
-  )
-}
-
+  )}
 export default TaskListNumbers
